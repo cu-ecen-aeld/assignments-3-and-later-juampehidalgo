8b73645425665f8f79d9d7e53c2869804b966c97
juampehidalgo <juanpedro.hidalgo@hotmail.com>
2023-05-26 09:52:16 +0300

    Modifying start/stop script to look more like dropbear start/stop script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,28 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
 # Description: start/stop init script for the aesd socket server application assignment
 
 do_start() {
-    start-stop-daemon --start --startas /usr/bin/aesdsocket -- -d
+    AESDSOCKET_ARGS="$AESDSOCKET_ARGS -d"
+    printf "Starting aesdsocket as daemon..."
+    umask 077
+    
+    start-stop-daemon -S -q -p /var/run/aesdsocket.pid --exec /usr/bin/aesdsocket -- $AESDSOCKET_ARGS
+    [ $? = 0 ] && echo "OK" || echo "FAIL"
 }
 
 do_stop() {
-    start-stop-daemon --stop --name aesdsocket --signal SIGTERM
-}
-
-do_status() {
-    start-stop-daemon --status --name aesdsocket && exit_status=$? || exit_status=$?
-    case "$exit_status" in
-        0)
-            echo "aesdsocket is running."
-            ;;
-        3)
-            echo "aesdsocket is not running."
-            ;;
-        4)
-            echo "Unable to determine aesdsocket status."
-            ;;
-    esac
+    printf "Stopping aesdsocket..."
+    start-stop-daemon -K -q -p /var/run/aesdsocket.pid -s SIGTERM
+    [ $? = 0 ] && echo "OK" || echo "FAIL"
 }
 
 case "$1" in
@@ -35,9 +27,6 @@ case "$1" in
     restart)
         do_stop
         do_start
-        ;;
-    status)
-        do_status
         ;;
     *)
         echo "Usage: $0 {start|stop|restart}"
